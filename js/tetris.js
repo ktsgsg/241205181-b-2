226e84ee1330463a509c8da30c864a68d14bd6d2
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-05-19 14:36:58 +0900

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'black', 'black', 'black', 'black', 'black', 'black', 'black'
+    'green', 'black', 'black', 'black', 'black', 'black', 'black'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
